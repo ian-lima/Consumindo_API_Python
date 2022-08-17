7aed414d25cfc567957b6428db9f4dd592f20829
Ian Lima <ianmarialima@gmail.com>
2022-08-17 14:05:23 -0300

    Consumindo API com Python

---
+++ consumir_api_python/api.py
@@ -0,0 +1,9 @@
+# Consumindo API da cotação do dólar
+
+import requests
+import json
+
+cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+cotacoes = cotacoes.json()
+cotacao_dolar = cotacoes['USDBRL']["bid"]
+print(cotacao_dolar)
